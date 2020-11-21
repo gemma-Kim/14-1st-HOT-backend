f8b1866d3e5bccf7b0134c9e3144e610e0292d1b
Dooyeon Kang <dyhouse0517@gmail.com>
2020-11-21 23:07:01 +0900

    Add: PostDetailView 엔드포인트 구현

---
+++ post/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import PostDetailView
+
+urlpatterns = [
+    path('/<int:post_id>', PostDetailView.as_view()),
+]
---
+++ post/views.py
@@ -0,0 +1,58 @@
+import json
+
+from django.http      import JsonResponse
+from django.views     import View
+from django.db.models import Q
+
+from post.models import Post, PostImage, ProductInPost, Comment, Tag, PostTag
+from user.models import User
+
+class PostDetailView(View):
+    def get(self, request, post_id):
+        try:
+            post = Post.objects.\
+                    prefetch_related(
+                        'posttag_set',
+                        'posttag_set__tag',
+                        'postimage_set',
+                        'comment_set',
+                        'comment_set__author'
+                    ).select_related('author').get(id=post_id)
+        except Exception as error_message:
+            return JsonResponse({'message': 'INVALID_POST'}, status = 400)
+
+        result = {
+                    'post_id'    : post.id,
+                    'content'    : post.content,
+                    'created_at' : post.created_at,
+                    'updated_at' : post.updated_at,
+                    'author' : {
+                        'author_id'     : post.author.id,
+                        'username'      : post.author.username,
+                        'profile_image' : post.author.profile_image_url
+                    },
+                    'tags' : [ tag.tag.name for tag in post.posttag_set.all() ],
+                    'comments' : [
+                        {
+                            'id': comment.id,
+                            'author': {
+                                'author_id'     : comment.author.id,
+                                'username'      : comment.author.username,
+                                'profile_image' : comment.author.profile_image_url
+                            },
+                            'content'    : comment.content,
+                            'created_at' : comment.created_at,
+                            'updated_at' : comment.updated_at,
+                            'parent_id'  : comment.parent_id,
+                        }
+                        for comment in post.comment_set.all()
+                    ],
+                    'post_images' : [
+                        {
+                            'image_url' : postimage.image_url
+                        }
+                        for postimage in post.postimage_set.all()
+                    ]
+        }
+
+        return JsonResponse({'results' : result}, status = 200)
