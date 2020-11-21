b1b5db9d614813b53f9892036f14cd55f122219e
Dooyeon Kang <dyhouse0517@gmail.com>
2020-11-21 22:25:21 +0900

    Add: Post 생성 엔드포인트 구현, Product연결 제외

---
+++ post/views.py
@@ -0,0 +1,47 @@
+
+import json
+
+from django.http import JsonResponse
+from django.views import View
+from django.db.models import Q
+
+from post.models import Post, PostImage, ProductInPost, Comment, Tag, PostTag
+from user.models import User
+from user.utils import login_decorator
+
+class PostView(View):
+#    @login_decorator
+    def post(self, request):
+        data = json.loads(request.body)
+        user = User.objects.get(id=1)
+
+        if not 'content' in data or not 'images' in data or not 'tags' in data:
+            return JsonResponse({'message':'KEY_ERROR'}, status=400)
+
+        created_post = Post.objects.create(
+            content = data['content'],
+            author = user
+        )
+
+        try:
+            for image in data['images']:
+                PostImage.objects.create(
+                    post = created_post,
+                    image_url = image
+                )
+
+        except TypeError:
+            return JsonResponse({'message':'IMAGES_TYPE_ERROR'}, status=400)
+
+        try:
+            for tag in data['tags']:
+                created_tag = Tag.objects.create(name = tag)
+                PostTag.objects.create(
+                    tag = created_tag,
+                    post = created_post
+                )
+
+            return JsonResponse({'message':'SUCCESS'}, status=200)
+
+        except TypeError:
+            return JsonResponse({'message':'TAGS_TYPE_ERROR'}, status=400)
