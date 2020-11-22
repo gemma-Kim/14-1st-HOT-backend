c7308e266157bc8f37c151d26ca9ecc0ad8dd57c
Gemma-Kim <gemmak.rok@gmail.com>
2020-11-23 04:13:19 +0900

    Add: mypage view

--- user/urls.py
+++ user/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views      import RegisterView, SignInView, FollowView, LikeView, BookmarkView
+from .views      import RegisterView, SignInView, LikeView, BookmarkView, MyPageView #FollowView
 
 
 urlpatterns = [
     path('/register', RegisterView.as_view()),
     path('/signin', SignInView.as_view()),
-    path('/follow', FollowView.as_view()),
-    path('/like', LikeView.as_view(),
-    path('/bookmark', BookmarkView.as_view())
+    #path('/follow', FollowView.as_view()),
+    path('/like', LikeView.as_view()),
+    path('/bookmark', BookmarkView.as_view()),
+    path('/mypage', MyPageView.as_view())
 ]
--- user/views.py
+++ user/views.py
@@ -5,7 +5,9 @@ from django.views     import View
 from django.http      import JsonResponse
 
 from my_settings      import SECRET_KEY, ALGORITHM
-from user.models      import User, Follow, Like, PostBookmark, ProductBookmark, CollectionBookmark
+from user.models      import User, Follow, Like, PostBookmark, ProductBookmark
+from product.models   import ProductImage
+from post.models      import PostImage
 from user.utils       import login_decorator
 
 
@@ -173,7 +175,7 @@ class BookmarkView(View):
 
                 return JsonResponse({'message':'SUCCESS'}, status=200)
 
-            if CollectionBookmark.objects.filter(user_id=user.id, collection_id=data['collection_id'].exists():
+            if CollectionBookmark.objects.filter(user_id=user.id, collection_id=data['collection_id']).exists():
                 return JsonResponse({'message':'INVALID_BOOKMARK'}, status=400)
 
             CollectionBookmark.objects.create(user_id=user.id, collection_id=data['collection_id'])
@@ -198,19 +200,74 @@ class BookmarkView(View):
                 return JsonResponse({'message':'SUCCESS'}, status=200)
 
             elif data['product_id']:
-                elif not ProductBookmark.objects.filter(user_id=user.id, product_id=data['product_id']).exists():
+                if not ProductBookmark.objects.filter(user_id=user.id, product_id=data['product_id']).exists():
                     return JsonResponse({'messgae':'INVALID_BOOKMARK'}, status=400)
 
                 ProductBookmark.objects.filter(user_id=user.id, product_id=data['product_id']).delete()
 
                 return JsonResponse({'message':'SUCCESS'}, status=200)
 
-            if not CollectionBookmark.objects.filter(user_id=user.id, collection_id=data['collection_id'].exists():
+            if not CollectionBookmark.objects.filter(user_id=user.id, collection_id=data['collection_id']).exists():
                 return JsonResponse({'message':'INVALID_BOOKMARK'}, status=400)
 
             CollectionBookmark.objects.filter(user_id=user.id, collection_id=data['collection_id']).delete()
 
             return JsonResponse({'message':'SUCCESS'}, status=200)
+
+        except KeyError:
+            return JsonResponse({'message':'KEY_ERROR'}, status=400)
+
+
+class MyPageView(View):
+    @login_decorator
+    def get(self, request):
+        try:
+            user     = User.objects.get(id=request.user.id)
+            posts    = Post.objects.select_related('author').prefetch_related('postbookmark_set', 'postimage_set', 'like_set')
+            products = Product.objects.prefetch_related('productbookmark_set', 'productimage_set')
+            collections = Product.objects.select_related('collection').prefetch_related('productbookmark_set', 'productimage_set')
+            context  = {
+                'username'            : user.username,
+                'user_image'          : user.profile_image_url,
+                'follower'            : Follow.objects.filter(followee_id=user.id).count(),
+                'following'           : Follow.objects.filter(follower_id=user.id).count(),
+                'bookmark_count'      : PostBookmark.objects.filter(user_id=user.id).count() + ProductBookmark.objects.filter(user_id=user.id).count(),
+                'like_count'          : Like.objects.filter(user_id=user.id).count(),
+                'bookmark_posts'      : [
+                    {
+                        'post_id'  : post.id,
+                        'image_url': post.postimage_set.first().image_url
+                    }
+                        for post in posts if PostBookmark.objects.filter(user_id=user.id, post_id=post.id)
+                ],
+                'bookmark_products'   : [
+                    {
+                        'product_id': product.id,
+                        'image_url' : product.product_images_set.first().product_image_url
+                    }
+                        for product in products if ProductBookmark.objects.filter(user_id=user.id, product_id=product.id)
+                ],
+                'bookmark_collections' : [
+                    {
+                        'collection_id': collection.id,
+                        'image_url'    : [
+                            productimage.product_image_url
+                                for productimage in collection.productimage_set.all()
+                        ]
+                    }
+                        for collection in collections if CollectionBookmark.objects.filter(user_id=user.id, collection_id=collection.id)
+                ],
+
+                'like'                 : [
+                    {
+                        'post_id'  : post.id,
+                        'image_url': post.postimage_set.first().image_url
+                    }
+                        for post in posts if Like.objects.filter(user_id=user.id, post_id=post.id)
+                ]
+            }
+
+            return JsonResponse({'context': context}, status=200)
 
         except KeyError:
             return JsonResponse({'message':'KEY_ERROR'}, status=400)
