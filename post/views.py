4748dbf506db1d54cfdbb3a1d58b6daaf6ee2a93
Dooyeon Kang <dyhouse0517@gmail.com>
2020-11-23 17:06:30 +0900

    Modified: 리뷰 반영

--- post/views.py
+++ post/views.py
@@ -22,58 +22,59 @@ class PostDetailView(View):
                         'postbookmark_set',
                         'postbookmark_set__user'
                     ).select_related('author').get(id=post_id)
-        except Exception as error_message:
+
+            result = {
+                        'post_id'         : post.id,
+                        'content'         : post.content,
+                        'created_at'      : post.created_at,
+                        'updated_at'      : post.updated_at,
+                        'likes_count'     : post.like_set.count(),
+                        'bookmarks_count' : post.postbookmark_set.count(),
+                        'comments_count'  : post.comment_set.count(),
+                        'likes_detail'    : [
+                            {
+                                'user_id'  : like.user.id,
+                                'username' : like.user.username
+                            }
+                            for like in post.like_set.all()
+                        ],
+                        'bookmarks_detail' : [
+                            {
+                                'user_id'  : bookmark.user.id,
+                                'username' : bookmark.user.username
+                            }
+                            for bookmark in post.postbookmark_set.all()
+                        ],
+                        'author' : {
+                            'author_id'     : post.author.id,
+                            'username'      : post.author.username,
+                            'profile_image' : post.author.profile_image_url
+                        },
+                        'tags'     : [ tag.tag.name for tag in post.posttag_set.all() ],
+                        'comments' : [
+                            {
+                                'id': comment.id,
+                                'author': {
+                                    'author_id'     : comment.author.id,
+                                    'username'      : comment.author.username,
+                                    'profile_image' : comment.author.profile_image_url
+                                },
+                                'content'    : comment.content,
+                                'created_at' : comment.created_at,
+                                'updated_at' : comment.updated_at,
+                                'parent_id'  : comment.parent_id,
+                            }
+                            for comment in post.comment_set.all()
+                        ],
+                        'post_images' : [
+                            {
+                                'image_url' : postimage.image_url
+                            }
+                            for postimage in post.postimage_set.all()
+                        ]
+            }
+
+            return JsonResponse({'results' : result}, status = 200)
+
+        except Post.DoesNotExist:
             return JsonResponse({'message': 'INVALID_POST'}, status = 400)
-
-        result = {
-                    'post_id'         : post.id,
-                    'content'         : post.content,
-                    'created_at'      : post.created_at,
-                    'updated_at'      : post.updated_at,
-                    'likes_count'     : post.like_set.count(),
-                    'bookmarks_count' : post.postbookmark_set.count(),
-                    'comments_count'  : post.comment_set.count(),
-                    'likes_detail'    : [
-                        {
-                            'user_id'  : like.user.id,
-                            'username' : like.user.username
-                        }
-                        for like in post.like_set.all()
-                    ],
-                    'bookmarks_detail' : [
-                        {
-                            'user_id'  : bookmark.user.id,
-                            'username' : bookmark.user.username
-                        }
-                        for bookmark in post.postbookmark_set.all()
-                    ],
-                    'author' : {
-                        'author_id'     : post.author.id,
-                        'username'      : post.author.username,
-                        'profile_image' : post.author.profile_image_url
-                    },
-                    'tags'     : [ tag.tag.name for tag in post.posttag_set.all() ],
-                    'comments' : [
-                        {
-                            'id': comment.id,
-                            'author': {
-                                'author_id'     : comment.author.id,
-                                'username'      : comment.author.username,
-                                'profile_image' : comment.author.profile_image_url
-                            },
-                            'content'    : comment.content,
-                            'created_at' : comment.created_at,
-                            'updated_at' : comment.updated_at,
-                            'parent_id'  : comment.parent_id,
-                        }
-                        for comment in post.comment_set.all()
-                    ],
-                    'post_images' : [
-                        {
-                            'image_url' : postimage.image_url
-                        }
-                        for postimage in post.postimage_set.all()
-                    ]
-        }
-
-        return JsonResponse({'results' : result}, status = 200)
