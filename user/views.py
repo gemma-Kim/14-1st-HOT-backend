858877426140a5d127b5ef3bbb2ea478b4c3ee5b
Gemma-Kim <gemmak.rok@gmail.com>
2020-11-24 18:29:10 +0900

    북마크, 좋아요 뷰 추가

--- user/views.py
+++ user/views.py
@@ -105,4 +105,106 @@ class UnFollowView(View):
             return JsonResponse({'message':'DELETED'}, status=200)
 
         except KeyError:
+            return JsonResponse({'message': "KEY_ERROR"}, status=200)
+
+
+class BookmarkView(View):
+    @login_decorator
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+            user = User.objects.get(id=request.user.id)
+
+            if 'post_id' in data:
+                if PostBookmark.objects.filter(user_id=user.id, post_id=data['post_id']).exists():
+                    return JsonResponse({'messsage':'INVALID_BOOKMARK'}, status=400)
+                print('a')
+                PostBookmark.objects.create(user_id=user.id, post_id=data['post_id'])
+                print('b')
+                return JsonResponse({'message':'SUCCESS'}, status=200)
+
+            elif 'product_id' in data:
+                if ProductBookmark.objects.filter(user_id=user.id, product_id=data['product_id']).exists():
+                    return JsonResponse({'messgae':'INVALID_BOOKMARK'}, status=400)
+                print('c')
+                ProductBookmark.objects.create(user_id=user.id, product_id=data['product_id'])
+                print('d')
+                return JsonResponse({'message':'SUCCESS'}, status=200)
+
+            if CollectionBookmark.objects.filter(user_id=user.id, collection_id=data['collection_id']).exists():
+                return JsonResponse({'message':'INVALID_BOOKMARK'}, status=400)
+            print('e')
+            CollectionBookmark.objects.create(user_id=user.id, collection_id=data['collection_id'])
+            print('f')
+            return JsonResponse({'message':'SUCCESS'}, status=200)
+
+        except KeyError:
+            return JsonResponse({'message':'KEY_ERROR'}, status=400)
+
+
+class UnBookmarkView(View):
+     @login_decorator
+     def post(self, request):
+         try:
+             data = json.loads(request.body)
+             user = User.objects.get(id=request.user.id)
+
+             if 'post_id' in data:
+                 if not PostBookmark.objects.filter(user_id=user.id, post_id=data['post_id']).exists():
+                     return JsonResponse({'messsage':'INVALID_DELETE'}, status=400)
+
+                 PostBookmark.objects.filter(user_id=user.id, post_id=data['post_id']).delete()
+
+                 return JsonResponse({'message':'SUCCESS'}, status=200)
+
+             elif 'product_id' in data:
+                 if not ProductBookmark.objects.filter(user_id=user.id, product_id=data['product_id']).exists():
+                     return JsonResponse({'messgae':'INVALID_BOOKMARK'}, status=400)
+
+                 ProductBookmark.objects.filter(user_id=user.id, product_id=data['product_id']).delete()
+
+                 return JsonResponse({'message':'SUCCESS'}, status=200)
+
+             if not CollectionBookmark.objects.filter(user_id=user.id, collection_id=data['collection_id']).exists():
+                 return JsonResponse({'message':'INVALID_BOOKMARK'}, status=400)
+
+             CollectionBookmark.objects.filter(user_id=user.id, collection_id=data['collection_id']).delete()
+
+             return JsonResponse({'message':'SUCCESS'}, status=200)
+
+         except KeyError:
+             return JsonResponse({'message':'KEY_ERROR'}, status=400)
+
+
+class LikeView(View):
+    @login_decorator
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+            user = User.objects.get(id=request.user.id)
+
+            if Like.objects.filter(user_id=user.id, post_id=data['id']).exists():
+                return JsonResponse({'message':'INVALID_LIKE'}, status=400)
+
+            Like.objects.create(user_id=user.id, post_id=data['id'])
+
+            return JsonResponse({'message':'SUCCESS'}, status=200)
+
+        except KeyError:
+            return JsonResponse({'message':"KEY_ERROR"}, status=400)
+
+    @login_decorator
+    def delete(self, request):
+        try:
+            data = json.loads(request.body)
+            user = User.objects.get(id=request.user.id)
+
+            if not Like.objects.filter(user_id=user.id, post_id=data['id']).exists():
+                return JsonResponse({'message':'INVALID_DELETE'}, status=400)
+
+            Like.objects.filter(user_id=user.id, post_id=data['id']).delete()
+
+            return JsonResponse({'message':'SUCCESS'}, status=200)
+
+        except KeyError:
             return JsonResponse({'message':'KEY_ERROR'}, status=400)
