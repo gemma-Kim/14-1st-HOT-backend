5639dd9fab22499f6c5f35f49dda44ed94742c04
Gemma-Kim <gemmak.rok@gmail.com>
2020-11-25 14:37:24 +0900

    mypage 뷰 추가

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views      import RegisterView, LogInView, FollowView, UnFollowView
+from .views      import RegisterView, LogInView, FollowView, UnFollowView, BookmarkView, UnBookmarkView, MyPageView
 
 
 urlpatterns = [
     path('/register', RegisterView.as_view()),
     path('/login', LogInView.as_view()),
     path('/follow', FollowView.as_view()),
-    path('/unfollow', UnFollowView.as_view())
+    path('/unfollow', UnFollowView.as_view()),
+    path('/bookmark', BookmarkView.as_view()),
+    path('/unbookmark', UnBookmarkView.as_view()),
+    path('/mypage', MyPageView.as_view())
 ]
