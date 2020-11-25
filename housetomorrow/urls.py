670781b92877b4b5e35fce9aeeb670f4813491ea
Gemma-Kim <gemmak.rok@gmail.com>
2020-11-26 04:21:54 +0900

    Add: order view

--- housetomorrow/urls.py
+++ housetomorrow/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('user', include('user.urls')),
     path('store', include('product.urls')),
     path('posts', include('post.urls')),
+    path('order', include('order.urls'))
 ]
