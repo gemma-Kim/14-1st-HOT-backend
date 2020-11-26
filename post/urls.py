f522fab84e135b47bd8688050325f9e121846cf4
Dooyeon Kang <dyhouse0517@gmail.com>
2020-11-26 19:30:40 +0900

    Add: 댓글 삭제기능 추가, 댓글 get 메소드 작성

--- post/urls.py
+++ post/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import CommentView, PostView, PostDetailView
+from .views import CommentView, CommentModifyView, PostView, PostDetailView
 
 urlpatterns = [
     path('/<int:post_id>', PostDetailView.as_view()),
     path('', PostView.as_view()),
     path('/<int:post_id>/comments', CommentView.as_view()),
-    path('/<int:post_id>/comments/<int:comment_id>', CommentView.as_view()),
+    path('/<int:post_id>/comments/<int:comment_id>', CommentModifyView.as_view()),
 ]
