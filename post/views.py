6efdc17e0ed80c9e66067b0d6b8de32af7f82205
Dooyeon Kang <dyhouse0517@gmail.com>
2020-11-29 17:02:45 +0900

    Add: Post 리스트 불러올 때 sort 기능 추가

--- post/views.py
+++ post/views.py
@@ -1,9 +1,10 @@
 import json
 
-from django.http      import JsonResponse
-from django.views     import View
-from django.db.models import Q, Prefetch
-from django.db        import transaction
+from django.http            import JsonResponse
+from django.views           import View
+from django.db.models       import Q, Count
+from django.db              import transaction
+from django.core.exceptions import FieldError
 
 from post.models import Post, PostImage, ProductInPost, Comment, Tag, PostTag
 from user.models import User, Like, PostBookmark
@@ -11,55 +12,64 @@ from user.utils  import login_decorator
 
 class PostView(View):
     def get(self, request):
-        posts = Post.objects.prefetch_related(
-            'like_set',
-            'like_set__user',
-            'postbookmark_set',
-            'postbookmark_set__user',
-            'comment_set',
-            'comment_set__author',
-            'postimage_set',
-        ).select_related('author').all().order_by('-id')
+        try:
+            sortby = request.GET.get('sort', None)
+            posts = Post.objects.prefetch_related(
+                'like_set',
+                'like_set__user',
+                'postbookmark_set',
+                'postbookmark_set__user',
+                'comment_set',
+                'comment_set__author',
+                'postimage_set',
+            ).select_related('author').all().order_by('-id')
 
-        results = [
-            {
-                'post_id'              : post.id,
-                'post_content'         : post.content,
-                'post_author_id'       : post.author.id,
-                'post_author_username' : post.author.username,
-                'post_author_profile'  : post.author.profile_image_url,
-                'post_mainimage_url'   : post.postimage_set.all()[0].image_url,
-                'post_likes_count'     : post.like_set.count(),
-                'post_bookmarks_count' : post.postbookmark_set.count(),
-                'post_comments_count'  : post.comment_set.count(),
-                'likes_detail' : [
-                    {
-                        'user_id'  : like.user.id,
-                        'username' : like.user.username
-                    }
-                    for like in post.like_set.all()
-                ],
-                'bookmarks_detail' : [
-                    {
-                        'user_id'  : bookmark.user.id,
-                        'username' : bookmark.user.username
-                    }
-                    for bookmark in post.postbookmark_set.all()
-                ],
-                'comments' : [
-                    {
-                        'author_id'       : comment.author.id,
-                        'author_username' : comment.author.username,
-                        'author_profile'  : comment.author.profile_image_url,
-                        'content'         : comment.content,
-                    }
-                    for comment in post.comment_set.all()
-                ],
-            }
-            for post in posts
-        ]
+            #sort by a number of likes | bookmarks      | comments
+            #query str example  'like' |'postbookmark' | 'comment'
+            posts = posts.annotate(count=Count(sortby)).order_by('-count','-id')
 
-        return JsonResponse({'result' : results}, status = 200)
+            results = [
+                {
+                    'post_id'              : post.id,
+                    'post_content'         : post.content,
+                    'post_author_id'       : post.author.id,
+                    'post_author_username' : post.author.username,
+                    'post_author_profile'  : post.author.profile_image_url,
+                    'post_mainimage_url'   : post.postimage_set.all()[0].image_url,
+                    'post_likes_count'     : post.like_set.count(),
+                    'post_bookmarks_count' : post.postbookmark_set.count(),
+                    'post_comments_count'  : post.comment_set.count(),
+                    'likes_detail' : [
+                        {
+                            'user_id'  : like.user.id,
+                            'username' : like.user.username
+                        }
+                        for like in post.like_set.all()
+                    ],
+                    'bookmarks_detail' : [
+                        {
+                            'user_id'  : bookmark.user.id,
+                            'username' : bookmark.user.username
+                        }
+                        for bookmark in post.postbookmark_set.all()
+                    ],
+                    'comments' : [
+                        {
+                            'author_id'       : comment.author.id,
+                            'author_username' : comment.author.username,
+                            'author_profile'  : comment.author.profile_image_url,
+                            'content'         : comment.content,
+                        }
+                        for comment in post.comment_set.all()
+                    ],
+                }
+                for post in posts
+            ]
+
+            return JsonResponse({'result' : results}, status = 200)
+
+        except FieldError:
+            return JsonResponse({'message': 'INVALID_SORTING'}, status=400)
 
 
 class PostDetailView(View):
