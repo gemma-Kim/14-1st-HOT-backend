e4a7cfd21ab0d5366d27347eec68b97ca5a0cb04
gemma-Kim <gemmak.rok@gmail.com>
2021-02-24 19:32:47 +0900

    Refac: MyPageView

--- user/views.py
+++ user/views.py
@@ -1,6 +1,7 @@
 import re, json, bcrypt, jwt
 
-from django.db.models import Q, Case, When
+from django.db        import transaction
+from django.db.models import Q
 from django.views     import View
 from django.http      import JsonResponse
 
@@ -12,6 +13,7 @@ from post.models      import Post
 
 
 class RegisterView(View):
+    @transaction.atomic
     def post(self, request):
         try:
             data = json.loads(request.body)
@@ -43,14 +45,15 @@ class RegisterView(View):
 
 
 class LogInView(View):
+    @transaction.atomic
     def post(self, request):
         try:
-            signin_data      = json.loads(request.body)
-            user             = User.objects.get(email=signin_data['email'])
-            email            = signin_data['email']
-            password         = signin_data['password']
+            data             = json.loads(request.body)
+            user             = User.objects.get(email=data.get('email'))
+            email            = data.get('email', "")
+            password         = data.get('password', "")
             email_validation = re.compile(r'^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
-
+     
             if not re.match(email_validation, email):
                 return JsonResponse({'message':'INVALID_EMAIL'}, status=400)
 
@@ -69,6 +72,7 @@ class LogInView(View):
 
 
 class FollowView(View):
+    @transaction.atomic
     @login_decorator
     def post(self, request, user_id):
         try:
@@ -84,11 +88,14 @@ class FollowView(View):
 
             return JsonResponse({'message':'SUCCESS'}, status=200)
 
+        except User.DoesNotExist:
+            return JsonResponse({'message': 'DOES_NOT_EXIST', 'error_message': '해당 사용자는 존재하지 않습니다.'})
         except KeyError:
             return JsonResponse({'message':'KEY_ERROR'}, status=400)
 
 
 class UnFollowView(View):
+    @transaction.atomic
     @login_decorator
     def post(self, request, user_id):
         try:
@@ -102,127 +109,161 @@ class UnFollowView(View):
 
             return JsonResponse({'message':'DELETED'}, status=200)
 
+        except User.DoesNotExist:
+            return JsonResponse({'message': 'DOES_NOT_EXIST', 'error_message': '해당 사용자는 존재하지 않습니다.'})
         except KeyError:
             return JsonResponse({'message': "KEY_ERROR"}, status=200)
 
 
 class BookmarkView(View):
+    @transaction.atomic
     @login_decorator
     def post(self, request):
         try:
-            data = json.loads(request.body)
+            data       = json.loads(request.body)
+            post       = data.get('post_id')
+            product    = data.get('product_id')
+            collection = data.get('collection_id')
 
-            # 게시물 
-            if data.get('post_id'):
+            # 게시물, 상물, 콜렉션이 모두 없는 경우
+            if not (post or product or collection):
+                return JsonResponse({'message':'INVALID_BOOKMARK', 'error_message':'북마크 컨텐츠가 없습니다.'}, status=400)
+
+            # 게시물
+            if post:
                 # 해당 게시물이 북마크 되어 있는지 확인
-                if PostBookmark.objects.filter(user_id=request.user.id, post_id=data['post_id']).exists():
-                    return JsonResponse({'message': 'INVALID_BOOKMARK', 'error_messsage':'이 게시물은 현재  북마크 되어 있는 게시물입니다.'}, status=400)
+                if PostBookmark.objects.filter(user_id=request.user.id, post_id=post).exists():
+                    return JsonResponse({'message':'INVALID_BOOKMARK', 'error_messsage':'이 게시물은 현재  북마크 되어 있는 게시물입니다.'}, status=400)
                 # 게시물 북마크
-                PostBookmark.objects.create(user_id=request.user.id, post_id=data['post_id'])
-                return JsonResponse({'message':'SUCCESS'}, status=200)
+                with transaction.atomic():
+                    PostBookmark.objects.create(user_id=request.user.id, post_id=post)
+                    return JsonResponse({'message':'SUCCESS'}, status=200)
 
             # 상품 
-            if data.get('product_id'):
+            if product:
                 # 해당 상품이 북마크 되어 있는지 확인
-                if ProductBookmark.objects.filter(user_id=request.user.id, product_id=data['product_id']).exists():
+                if ProductBookmark.objects.filter(user_id=request.user.id, product_id=product).exists():
                     return JsonResponse({'messgae':'INVALID_BOOKMARK', 'error_message':'이 상품은 현재 북마크 되어 있는 상품입니다.'}, status=400)
                 # 상품 북마크
-                ProductBookmark.objects.create(user_id=request.user.id, product_id=data['product_id'])
-                return JsonResponse({'message':'SUCCESS'}, status=200)
+                with transaction.atomic():
+                    ProductBookmark.objects.create(user_id=request.user.id, product_id=product)
+                    return JsonResponse({'message':'SUCCESS'}, status=200)
 
             # 콜렉션 
             # 해당 콜렉션이 북마크 되어 있는지 확인
-            if CollectionBookmark.objects.filter(user_id=request.user.id, collection_id=data['collection_id']).exists():
+            if CollectionBookmark.objects.filter(user_id=request.user.id, collection_id=collection).exists():
                 return JsonResponse({'message':'INVALID_BOOKMARK', 'error_message': '이 콜렉션은 현재 북마크 되어 있는 콜렉션입니다.'}, status=400)
             # 콜렉션 북마크
-            CollectionBookmark.objects.create(user_id=request.user.id, collection_id=data['collection_id'])
-            return JsonResponse({'message':'SUCCESS'}, status=200)
+            with transaction.atomic():
+                CollectionBookmark.objects.create(user_id=request.user.id, collection_id=collection)
+                return JsonResponse({'message':'SUCCESS'}, status=200)
 
         except KeyError:
             return JsonResponse({'message':'KEY_ERROR'}, status=400)
 
 
 class UnBookmarkView(View):
-     @login_decorator
-     def post(self, request):
-         try:
-             data = json.loads(request.body)
+    @transaction.atomic
+    @login_decorator
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+            post       = data.get('post_id')
+            product    = data.get('product_id')
+            collection = data.get('collection_id')
 
-             # 게시물
-             if data.get('post_id'):
-                 # 게시물 북마크 삭제
-                 post_bookmark = PostBookmark.objects.get(user_id=request.user.id, post_id=data['post_id'])
-                 post_bookmark.delete()
-                 return JsonResponse({'message':'SUCCESS'}, status=200)
+            # 게시물, 상품, 콜렉션이 없는 경우
+            if not (post or product or collection):
+                return JsonResponse({'message':'INVALID_UNBOOKMARK', 'error_message':'북마크 취소 컨텐츠가 없습니다.'}, status=400)
+
+            # 게시물
+            if post:
+                post_bookmark = PostBookmark.objects.get(user_id=request.user.id, post_id=post)
+                post_bookmark.delete()
+                return JsonResponse({'message':'SUCCESS'}, status=200)
 
              # 상품
-             if data.get('product_id'):
-                 product_bookmark =  ProductBookmark.objects.get(user_id=request.user.id, product_id=data['product_id'])
-                 product_bookmark.delete()
-                 return JsonResponse({'message':'SUCCESS'}, status=200)
+            if product:
+                product_bookmark = ProductBookmark.objects.get(user_id=request.user.id, product_id=product)
+                product_bookmark.delete()
+                return JsonResponse({'message':'SUCCESS'}, status=200)
 
-             # 콜렉션
-             collection_bookmark = CollectionBookmark.objects.filter(user_id=request.user.id, collection_id=data['collection_id'])
-             collection_bookmark.delete()
-             return JsonResponse({'message':'SUCCESS'}, status=200)
+            # 콜렉션
+            collection_bookmark = CollectionBookmark.objects.filter(user_id=request.user.id, collection_id=collection)
+            collection_bookmark.delete()
+            return JsonResponse({'message':'SUCCESS'}, status=200)
 
-         except DoesNotExist.PostBookmark:
-             return JsonResponse({'messsage':'INVALID_UNBOOKMARK'}, status=400)
-         except DoesNotExist.ProductBookmark:
-             return JsonResponse({'messsage':'INVALID_UNBOOKMARK'}, status=400)
-         except DoesNotExist.CollectionBookmark:
-             return JsonResponse({'messsage':'INVALID_UNBOOKMARK'}, status=400)
-         except KeyError:
-             return JsonResponse({'message':'KEY_ERROR'}, status=400)
+        except DoesNotExist.PostBookmark:
+            return JsonResponse({'messsage':'INVALID_UNBOOKMARK', 'error_message':'북마크 취소할 게시물이 없습니다.'}, status=400)
+        except DoesNotExist.ProductBookmark:
+            return JsonResponse({'messsage':'INVALID_UNBOOKMARK', 'error_message':'북마크 취소할 상품이 없습니다.'}, status=400)
+        except DoesNotExist.CollectionBookmark:
+            return JsonResponse({'messsage':'INVALID_UNBOOKMARK', 'error_message':'북마크 취소할 콜렉션이 없습니다.'}, status=400)
+        except KeyError:
+            return JsonResponse({'message':'KEY_ERROR'}, status=400)
 
 
 class MyPageView(View):
     @login_decorator
-    def get(self, request, user_id):
+    def get(self, request):
         try:
-            user                 = User.objects.get(id=user_id)
-            posts                = Post.objects.select_related('author').prefetch_related('postbookmark_set', 'postimage_set', 'like_set')
-            bookmark_posts       = PostBookmark.objects.select_related('post').filter(user_id=user.id).order_by('-id')
-            bookmark_products    = ProductBookmark.objects.select_related('product').filter(user_id=user.id).order_by('-id')
-            bookmark_collections = CollectionBookmark.objects.select_related('collection').filter(user_id=user.id).order_by('-id')
+            user                 = User.objects.get(id=request.user.id)
+            followers            = Follow.objects.filter(followee_id=user.id)
+            followings           = Follow.objects.filter(follower_id=user.id)
+            likes                = Like.objects.filter(user_id=request.user.id)
 
-            context  = {
+            bookmark_posts       = PostBookmark.objects\
+                                        .select_related('post').prefetch_related('postimage_set')\
+                                        .filter(user_id=user.id).order_by('-id')
+
+            bookmark_products    = ProductBookmark.objects\
+                                        .select_related('product').prefetch_related('productimage_set')\
+                                        .filter(user_id=user.id).order_by('-id')
+
+            bookmark_collections = CollectionBookmark.objects\
+                                        .select_related('collection').prefetch_related('product_set', 'productimage_set')\
+                                        .filter(user_id=user.id).order_by('-id')
+
+            like_posts           = Like.objects\
+                                        .select_related('post').prefetch_related('postimage_set')\
+                                        .filter(user_id=user.id).order_by('-id')
+            
+            context = {
                 'username'      : user.username,
                 'user_image'    : user.profile_image_url,
-                'follower'      : Follow.objects.filter(followee_id=user.id).count(),
-                'following'     : Follow.objects.filter(follower_id=user.id).count(),
+                'follower'      : followers.count(),
+                'following'     : followings.count(),
+                'like_count'    : likes.count(),
                 'bookmark_count': bookmark_posts.count() + bookmark_products.count() + bookmark_collections.count(), 
-                'like_count'    : Like.objects.filter(user_id=request.user.id).count(),
                 'bookmark_posts': [
                     {
                         'post_id'  : bookmark_post.post_id,
-                        'image_url': Post.objects.get(id=bookmark_post.post_id).postimage_set.first().image_url
+                        'image_url': bookmark_post.post.postimage_set.first().image_url
                     }
-                        for bookmark_post in bookmark_posts if bookmark_posts
+                        for bookmark_post in bookmark_posts
                 ],
                 'bookmark_products': [
                     {
                         'product_id': bookmark_product.product_id,
-                        'image_url' : Product.objects.get(id=bookmark_product.product_id).productimage_set.first().product_image_url
+                        'image_url' : bookmark_product.product.productimage_set.first().product_image_url
                     }
-                        for bookmark_product in bookmark_products if bookmark_products
+                        for bookmark_product in bookmark_products
                 ],
                 'bookmark_collections': [
                     {
                         'collection_id': bookmark_collection.collection_id,
-                        'image_url'    : Product.objects.filter(collection_id=bookmark_collection.collection_id).first().productimage_set.first().product_image_url
+                        'image_url'    : bookmark_collection.collection.product_set.first().productimage_set.first().product_image_url
                     }
-                        for bookmark_collection in bookmark_collections if bookmark_collections
+                        for bookmark_collection in bookmark_collections if bookmark_collection.collection.product_set.first()
                 ],
-                'like'                : [
+                'like': [
                     {
-                        'post_id'  : post.id,
-                        'image_url': post.postimage_set.first().image_url
-                    }
-                        for post in posts if Like.objects.filter(user_id=user.id, post_id=post.id)
+                        'post_id'  : like_post.post.id,
+                        'image_url': like_post.post.postimage_set.first().image_url
+                    }   
+                        for like_post in like_posts if like_post.post
                 ]
             }
-
             return JsonResponse({'context': context}, status=200)
 
         except User.DoesNotExist:
